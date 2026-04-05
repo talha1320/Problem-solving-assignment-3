0198fce258d0fda93ed89604093148f7db87bb24
talha_1320 <abutalhamdfahad@gmail.com>
2026-04-05 13:45:33 +0600

    solved problem1

--- problem2.js
+++ problem2.js
@@ -1,4 +1,14 @@
 /** Problem -02 ( Cycle or Laptop ) */
-var money = 10000;
+var money = 9999;
 //write your code here
 var result;
+if (money >= 45000) {
+    result = "Laptop";
+}   
+else if (money >= 10000)  {
+    result = "Cycle";
+}
+else {
+    result = "Chocolate";
+}
+console.log(result);
