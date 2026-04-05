82be6b623331adb1d4fdd7285decd62d0a3855f0
talha_1320 <abutalhamdfahad@gmail.com>
2026-04-05 12:57:42 +0600

    solved problem1

---
+++ final.js
@@ -0,0 +1,7 @@
+/*** Problem -01 ( Divide the Asset ) */
+var area = 800;
+
+// write your code here
+var result = area / 2;
+
+console.log(result);
---
+++ problem2.js
@@ -0,0 +1,4 @@
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+var result;
