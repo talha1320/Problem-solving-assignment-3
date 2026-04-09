a3443687d42216e14a62d7700f332baeb60e4402
talha_1320 <abutalhamdfahad@gmail.com>
2026-04-09 16:19:04 +0600

    solved problem5 and added to finals

--- final.js
+++ final.js
@@ -48,3 +48,12 @@ if (
 }
 
 console.log(fileName);
+
+
+/** Problem 05 - ( PH Email Generator ) */
+var student = { name: "jhankar", roll: 1014, department: "cse" };
+
+// write your code here
+var email = student.name + student.roll + "." + student.department + "@ph.ac.bd";
+
+console.log(email);
